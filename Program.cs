107010a3e4367a7c8b3d56fc1cc87246afd963f6
Dmitry <markelov16@gmail.com>
2022-10-22 21:39:33 +0500

    1 задача

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// вариант решения 1 задачи
+bool a = true;
+int count = 0;
+while (a!=false){
+    Console.WriteLine("введите число");
+    int nums = int.Parse(Console.ReadLine()!);
+    if (nums>0){
+    count++;
+    }
+    Console.WriteLine("Для продолжения введите (true), для остановки (false)");
+    bool b = bool.Parse(Console.ReadLine()!);
+    a = b;
+}
+Console.WriteLine($"количество введеных чисел больше 0 = {count}");
